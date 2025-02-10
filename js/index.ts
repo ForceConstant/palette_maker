c7d5f9bbd1dc6ac843ba9211878f66ec26c6c893
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2025-02-10 19:27:23 +0000

    started using a bundler

---
+++ js/index.ts
@@ -0,0 +1,8 @@
+import "@melloware/coloris";
+import "@melloware/coloris/dist/coloris.css";
+import Alpine from "alpinejs";
+
+//@ts-ignore
+window.Alpine = Alpine;
+
+Alpine.start();
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./docs/**/*.{html,js}", "index.html"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
